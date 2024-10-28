2c0a14d72be293870ccc79baa6956a278e5a1c28
SowonBae <engel5526@naver.com>
2024-10-28 23:50:13 +0900

    1. 라우팅 설정 수정 2. axios 공통 hook 추가 3. Auth Provider 추가 4. 토큰 자동 관리 로직 추가

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,11 +12,58 @@ import BuyerList from "../pages/manages/BuyerList.jsx";
 import UserList from "../pages/manages/UserList.jsx";
 import ItemList from "../pages/manages/ItemList.jsx";
 import PriceList from "../pages/manages/PriceList.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
+import NotFound from "../pages/error/Notfound.jsx";
+import Report from "../pages/Report.jsx";
 
-function AppRoutes() {
+const AppRoutes = () => {
     return (
         <Routes>
+            {/* 로그인 */}
+            <Route path={`/login`} element={<Login/>}/>
 
+            {/* 메인 */}
+            <Route path={`/`} element={<PrivateRoute/>}>
+                <Route index={true} element={<Report/>}/>
+            </Route>
+
+            {/* 리포트 */}
+            <Route path={`/report`} element={<PrivateRoute/>}>
+                <Route index={true} element={<Report/>}/>
+            </Route>
+
+            {/* 오더 */}
+            <Route path={`/order`} element={<PrivateRoute/>}>
+                <Route index={true}/>
+            </Route>
+            <Route path={`/order/detail`} element={<PrivateRoute/>}>
+                <Route index={true}/>
+            </Route>
+
+            {/* 바이어관리 */}
+            <Route path={`/buyer`} element={<PrivateRoute/>}>
+                <Route index={true} element={<BuyerList/>}/>
+            </Route>
+
+            {/* 직원관리 */}
+            <Route path={`/user`} element={<PrivateRoute/>}>
+                <Route index={true} element={<UserList/>}/>
+            </Route>
+
+            {/* 판매부번관리 */}
+            <Route path={`/item`} element={<PrivateRoute/>}>
+                <Route index={true} element={<ItemList/>}/>
+            </Route>
+
+            {/* 바이어별 판매가격 관리 */}
+            <Route path={`/price`} element={<PrivateRoute/>}>
+                <Route index={true} element={<PriceList/>}/>
+            </Route>
+
+            {/* Not Found */}
+            <Route path="*" element={<NotFound/>}/>
+
+            {/* 테스트 중 */}
             <Route path={`/sidebar`} element={<Sidebar/>}/>
             <Route path={`/header`} element={<Header/>}/>
             <Route path={`/userTables`} element={<UserTables/>}/>
@@ -24,37 +71,9 @@ function AppRoutes() {
             <Route path={`/priceTables`} element={<PriceTables/>}/>
             <Route path={`/itemTables`} element={<ItemTables/>}/>
             <Route path={`/input`} element={<Input/>}/>
-
-            {/*로그인*/}
-            <Route path={`/login`} element={<Login/>}/>
             <Route path={`/pass`} element={<PassChange/>}/>
-
-            {/*리포트*/}
-            <Route path={`/report`}/>
-
-            {/*오더*/}
-            <Route path={`/order`}/>
-            <Route path={`/order/detail`}/>
-
-
-            {/*바이어관리*/}
-            <Route path={`/buyer`} element={<BuyerList />}/>
-
-
-            {/*직원관리*/}
-            <Route path={`/user`} element={<UserList />}/>
-
-
-            {/*판매부번관리*/}
-            <Route path={`/item`} element={<ItemList />}/>
-
-
-            {/*바이어별 판매가격 관리*/}
-            <Route path={`/price`} element={<PriceList />}/>
-
-
         </Routes>
-    )
-}
+    );
+};
 
 export default AppRoutes;
