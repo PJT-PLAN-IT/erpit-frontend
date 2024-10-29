12c52e0747ff843c91ccac16ae27abc5e476293e
SowonBae <engel5526@naver.com>
2024-10-29 23:40:30 +0900

    add 유저, 바이어, 판매가, 바이어별 가격 조회 추가

--- src/pages/manages/BuyerList.jsx
+++ src/pages/manages/BuyerList.jsx
@@ -1,11 +1,15 @@
 import BuyerTables from "../../components/tables/BuyerTables.jsx";
 import Input from "../../components/items/Input.jsx";
 import Buttons from "../../components/items/Buttons.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import BuyerInsert from "../../components/modal/BuyerInsert.jsx";
+import useAxios from "../../hook/useAxios.js";
 
 function BuyerList() {
-
+    const { error, fetchData } = useAxios();
+    const [basicParam, setbasicParam] = useState();
+    // {buyer : "1"}
+    const [result, setResult] = useState([]);
     const [insertModalOpen, setInsertModalOpen] = useState(false);
     const openInsertModal= () => {
         setInsertModalOpen(prev => !prev);
@@ -16,20 +20,42 @@ function BuyerList() {
         console.log("--------저장코드-------");
     }
 
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+
+    const getData = async () => {
+        try {
+            const resultData = await fetchData({
+                config: { method: "GET", url: "/api/buyer/list" },
+                params: basicParam === null ? null : basicParam
+            });
+            if (resultData) {
+                setResult(resultData.data);
+            } else if (error) {
+                console.error("Error: ", error);
+            }
+        } catch (err) {
+            console.error("Error: ", err);
+        }
+    };
+
+
 
     return (
-        <>
-            <div className={`flex justify-between mb-2`}>
+        <div className={`flex flex-col p-10`}>
+            <div className={`flex justify-between mb-2 mt-10`}>
                 <div className={`flex`}>
                     <Input search={'buyer'}/>
                     <Buttons style={`green-sm`} word={`search`}/>
                 </div>
                 <Buttons style={`green-sm`} word={`add`} onClick={openInsertModal}/>
             </div>
-            <BuyerTables/>
+            <BuyerTables data={result}/>
             <BuyerInsert isOpen={insertModalOpen} onClose={openInsertModal} onSave={saveFunction}/>
-
-        </>
+        </div>
     )
 }
 
--- src/pages/manages/ItemList.jsx
+++ src/pages/manages/ItemList.jsx
@@ -1,9 +1,45 @@
 import Input from "../../components/items/Input.jsx";
 import Buttons from "../../components/items/Buttons.jsx";
 import ItemTables from "../../components/tables/ItemTables.jsx";
+import useAxios from "../../hook/useAxios.js";
+import {useEffect, useState} from "react";
 
 function ItemList() {
+    const { error, fetchData } = useAxios();
+    const [basicParam, setbasicParam] = useState(null);
+    const [result, setResult] = useState([]);
+    // const [insertModalOpen, setInsertModalOpen] = useState(false);
+    // const openInsertModal= () => {
+    //     setInsertModalOpen(prev => !prev);
+    // };
+    //
+    // const saveFunction = () => {
+    //     openInsertModal();
+    //     console.log("--------저장코드-------");
+    // }
 
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+
+    const getData = async () => {
+        try {
+            const resultData = await fetchData({
+                config: { method: "GET", url: "/api/item/list" },
+                params: basicParam === null ? null : basicParam
+            });
+            if (resultData) {
+                console.log("dld",resultData)
+                setResult(resultData.data);
+            } else if (error) {
+                console.error("Error: ", error);
+            }
+        } catch (err) {
+            console.error("Error: ", err);
+        }
+    };
 
     return (
         <>
@@ -14,7 +50,7 @@ function ItemList() {
                 </div>
                 <Buttons style={`green-sm`} word={`add`}/>
             </div>
-            <ItemTables/>
+            <ItemTables result={result}/>
         </>
     )
 }
