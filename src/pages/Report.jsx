73264522a7eda53628c49584f1234b5201254ca2
SowonBae <engel5526@naver.com>
2024-11-01 15:47:32 +0900

    fix report page

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,18 +1,16 @@
 import { useAuth } from "../context/AuthContext.jsx";
+import {Link} from "react-router-dom";
 const Report = () => {
   const { user } = useAuth();
 
   return (
-    <>
-      <div className={`flex-col justify-center align-middle items-center`}>
-          <h1 className={`text-6xl`}>
-          공사중
-          </h1>
-          <p>
-              좌측에서 메뉴를 선택해주세요
-          </p>
-      </div>
-    </>
+      <>
+          <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
+              <h1 className="text-6xl font-bold mb-4">404</h1>
+              <h2 className="text-2xl font-semibold mb-2">공사중입니다</h2>
+              <p className="text-lg mb-6">좌측 메뉴를 선택해주세요</p>
+          </div>
+      </>
   );
 };
 
