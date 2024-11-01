f5ff05281796a721eb35d3ecd0f0ae397b90a1d2
SowonBae <engel5526@naver.com>
2024-11-01 10:01:37 +0900

    add 로그아웃

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
-import { Navigate } from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import useAxios from "../hook/useAxios.js";
 const Header = () => {
   const { user } = useAuth();
+  const {error, fetchData} = useAxios();
+
   const userIcon = (
     <FontAwesomeIcon icon={faCircleUser} className="text-gray-400 text-5xl" />
   );
 
-  const logout = () => {
-    <Navigate to="/login" replace />;
+  const navigator = useNavigate();
+  const logout = async () => {
+    if(localStorage.getItem('Access-Token')){
+      try {
+          const resultData = await fetchData({
+            config: { method: "POST", url: "/api/auth/logout" },
+          });
+        if (resultData) {
+          alert("로그아웃되었습니다.");
+          document.cookie = 'Refresh-Token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; // Refresh-Token 삭제
+          localStorage.removeItem('Access-Token'); // Access-Token 삭제
+          navigator('/login');
+        }
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    }
   };
 
   return (
